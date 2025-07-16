4cbe8af37145ce651b8b585cc9b68a4a45fc548a
Oliver Levine <oliversfphillips@icloud.com>
2025-07-16 09:43:46 -0700

    added a new route to get the total of the calculator

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
+// track total of our calculator
+let total = 0;
 
-
-
+// get route --> shows total of the calculator
+app.get('/calculator', (req, res) => {
+  // add logic here to return the total
+  // return the current total of the calculator
+  res.json({ total });
+});
 
 
 
